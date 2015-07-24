179843af36ffc18dc6f1e28bc5068a63a0f564b8
EyePulp <eyepulp@eyepulp.net>
2015-07-24 18:55:21 -0500

    change our xml parsing behavior

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,21 @@
 import xml2js from 'xml2js';
 import expressXmlParser from 'express-xml-bodyparser';
 
-var parserOpts = {
+xml2js.processors.parseBooleans = xml2js.processors.parseBooleans || function(str) {
+    if (/^(?:true|false)$/i.test(str)) {
+        str = str.toLowerCase() === 'true';
+    }
+    return str;
+};
+
+var xmlParser = new xml2js.Parser({
     explicitArray: false,
     explicitRoot: false,
-    tagNameProcessors: [xml2js.processors.normalize, xml2js.processors.stripPrefix],
-    attrNameProcessors: [xml2js.processors.normalize],
-};
+    ignoreAttrs:true,
+    mergeAttrs:true,
+    valueProcessors: [xml2js.processors.parseNumbers, xml2js.processors.parseBooleans],
+    tagNameProcessors: [xml2js.processors.stripPrefix],
+});
 
 export var xmlParser = new xml2js.Parser(parserOpts);
 export var xmlBodyParser = new expressXmlParser(parserOpts);
